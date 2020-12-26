bc1b67df61fc687bf803123527ca032ca779189d
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2020-12-26 23:22:21 +0300

    wwwroot dosyası eklenip gerekli sürüm güncellemeleri yapıldı.

--- MarketApp.webUI/Startup.cs
+++ MarketApp.webUI/Startup.cs
@@ -22,6 +22,7 @@ namespace MarketApp.webUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
