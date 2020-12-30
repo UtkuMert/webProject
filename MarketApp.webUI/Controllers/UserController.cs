fce5987b3e96525620bafe0dc52f7044fa06b901
arif-yusuf-yilmaz3 <y.37ylmaz@gmail.com>
2020-12-30 22:58:34 +0300

    Ürün detay sayfası oluşturuldu, detay sayfasında id değeri alınan ürün gösterildi.

---
+++ MarketApp.webUI/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using MarketApp.DataAccess.Abstract;
+using MarketApp.Entity;
+using MarketApp.webUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarketApp.webUI.Controllers
+{
+    public class UserController : Controller
+    {
+        private IProductRepository _productService;
+        public UserController(IProductRepository productService)
+        {
+            _productService = productService;
+        }
+        public IActionResult Details(int? id)
+        {
+            if (id == null) // herhangi bir id değeri girilmemiş ise hata döndürür.
+            {
+                return NotFound();
+            }
+            Product product = _productService.GetById((int)id);
+            if (product == null)    // alınan id değerine karşılık bir ürün bulunamamışsa hata döndürür.
+            {
+                return NotFound();
+            }
+            return View(product); // id değeri bulunan ürün gönderilir.
+        }
+        public IActionResult List()     // Tüm kayıtlar listelenir.
+        {
+            return View(new ProductListModel()
+            {
+                Products = _productService.GetAll()  
+            }) ;
+        }
+    }
+}
